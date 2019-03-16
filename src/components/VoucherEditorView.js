53b03d0ca135c204c18876def9c2b6d649abbdae
Daniel Otsuka <dkotsuka@gmail.com>
2019-03-16 18:33:35 -0300

    add state controls for components

--- src/components/VoucherEditorView.js
+++ src/components/VoucherEditorView.js
@@ -1,37 +1,111 @@
 import React, {Component} from 'react'
 
 class VoucherEditorView extends Component {
+	state = {
+		campaign: "",
+		cade: "",
+		start: new Date().toISOString(),
+		end: new Date().toISOString(),
+		value: 0,
+		type: "P",
+		maxTimes: 0,
+		timesByUser: 0,
+		services: []
+	}
+	handleSubmit = () => {
+
+	}
+
+	onChangeTextInput = event => {
+		const target = event.target;
+		const value = target.value;
+		const name = target.name;
+		this.setState({	[name]: value });
+	}
+
+	onChangeCheckboxInput = event => {
+		const target = event.target;
+		const value = target.checked;
+		const name = target.name;
+
+		if (value) {
+			this.setState(prevState => {
+				const newState = prevState.services.push(name);
+				return newState;
+			});
+	    } else {
+			this.setState(prevState => {
+				const newState = prevState.services.filter(value => value !== name);
+				return { services: newState };
+			});
+	    }
+	}
+
+	serviceIsChecked = (service) => {
+		const list = this.state.services
+	    for (let i = 0; i < list.length; i++) {
+	        if (list[i] === service) {
+	            return true;
+	        }
+	    }
+
+	    return false;
+	}
+
 	render(){
 		return <div className="editor-container">
-			<form>
+			<form onSubmit={e => this.handleSubmit(e)}>
 				<h2>Create New Voucher</h2>
 				<div className="input-container">
 					<span>Campaign name:</span>
-					<input type="text"/>
+					<input 
+						name="campaign" 
+						type="text" 
+						value={this.state.campaign} 
+						onChange={this.onChangeTextInput}/>
 				</div>
 				<div className="input-container">
 					<span>Promotinal Code:</span>
-					<input type="text"/>
+					<input 
+						name="code"
+						type="text"
+						value={this.state.code} 
+						onChange={this.onChangeTextInput}/>
 				</div>
 				<div className="row-container">
 					<div className="number input-container">
 						<span>Start date:</span>
-						<input type="date"/>
+						<input 
+							name="start"
+							type="date"
+							value={this.state.start} 
+							onChange={this.onChangeTextInput}/>
 					</div>
 					<div className="number input-container">
 						<span>End date:</span>
-						<input type="date"/>
+						<input 
+							name="end"
+							type="date"
+							value={this.state.end} 
+							onChange={this.onChangeTextInput}/>
 					</div>
 				</div>
 				<div className="row-container">
 					<div className="number input-container">
 						<span>Discount value:</span>
-						<input type="number"/>
+						<input 
+							name="value"
+							type="number"
+							value={this.state.value} 
+							onChange={this.onChangeTextInput}/>
 					</div>
 					<div className="select-container">
-						<select>
-							<option>Percentual</option>
-							<option>Fixed Value</option>
+						<select 
+							name="type"
+							value={this.state.type} 
+							onChange={this.onChangeTextInput}>
+							<option value="P">Percentual</option>
+							<option value="V">Fixed Value</option>
 						</select>
 					</div>
 				</div>
@@ -39,19 +113,35 @@ class VoucherEditorView extends Component {
 					<span>Eligible Services:</span>
 					<div className="checkbox-container">
 						<label>
-							<input type="checkbox" value="option2" />
+							<input 
+								name="depilation"
+								type="checkbox"
+								checked={this.serviceIsChecked("depilation")}
+								onChange={this.onChangeCheckboxInput}/>
 							<span>Depilation</span>
 						</label>
 						<label>
-							<input type="checkbox" value="option2" />
+							<input
+								name="hair"
+								type="checkbox"
+								checked={this.serviceIsChecked("hair")}
+								onChange={this.onChangeCheckboxInput}/>
 							<span>Hair</span>
 						</label>
 						<label>
-							<input type="checkbox" value="option2" />
+							<input 
+								name="massage"
+								type="checkbox"
+								checked={this.serviceIsChecked("massage")}
+								onChange={this.onChangeCheckboxInput}/>
 							<span>Massage</span>
 						</label>
 						<label>
-							<input type="checkbox" value="option2" />
+							<input 
+								name="nails"
+								type="checkbox"
+								checked={this.serviceIsChecked("nails")}
+								onChange={this.onChangeCheckboxInput}/>
 							<span>Nails</span>
 						</label>
 					</div>
@@ -59,11 +149,19 @@ class VoucherEditorView extends Component {
 				<div className="row-container">
 					<div className="number input-container">
 						<span>Max vouchers:</span>
-						<input type="number"/>
+						<input 
+							name="maxTimes"
+							type="number"
+							value={this.state.maxTimes} 
+							onChange={this.onChangeTextInput}/>
 					</div>
 					<div className="number input-container">
 						<span>Max per user:</span>
-						<input type="number"/>
+						<input 
+							name="timesByUser"
+							type="number"
+							value={this.state.timesByUser} 
+							onChange={this.onChangeTextInput}/>
 					</div>
 				</div>
 			</form>		
